2967b00a6310df5697e994a824bdf7650e5cd646
Mahmoud Rawas <mhmoudr@gmail.com>
2022-09-27 19:49:26 +1000

    doing some progress ..

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -29,6 +28,11 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
+    let default_team = Team {
+        name:String::new(),
+        goals_scored:0,
+        goals_conceded:0
+    };
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
@@ -40,6 +44,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let score1 = scores
+            .entry(team_1_name.clone())
+            .or_insert(
+                Team {
+                    name: team_1_name,
+                    .. default_team
+                });
+        score1.goals_scored += team_1_score;
+        score1.goals_conceded += team_2_score;
+        let score2 = scores
+            .entry(team_2_name.clone())
+            .or_insert(
+                Team {
+                    name: team_2_name,
+                    .. default_team
+                });
+        score2.goals_scored += team_2_score;
+        score2.goals_conceded += team_1_score;
     }
     scores
 }
